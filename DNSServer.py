00760fc30f6023d6ec7eb26c904f3c5b282eed6c
wu-jiaxi <dev.jiaxiwu@gmail.com>
2026-03-21 19:18:04 -0400

    dgram

--- DNSServer.py
+++ DNSServer.py
@@ -81,7 +81,7 @@ dns_records = {
 
 def run_dns_server():
     # Create a UDP socket and bind it to the local IP address (what unique IP address is used here, similar to webserver lab) and port (the standard port for DNS)
-    server_socket = socket.socket(socket.AF_INET, socket.STREAM) # Research this
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Research this
     server_socket.bind(("", 13331))
 
     while True:

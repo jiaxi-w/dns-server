dcd3bd248cf4436f48777ce21e1a360ba0b23139
wu-jiaxi <dev.jiaxiwu@gmail.com>
2026-03-21 22:57:29 -0400

    nyu

--- DNSServer.py
+++ DNSServer.py
@@ -34,8 +34,8 @@ def generate_aes_key(password, salt):
 def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    encrypted_data = f.encrypt(input_string) #call the Fernet encrypt method
-    return encrypted_data.encode('utf-8')
+    encrypted_data = f.encrypt(input_string.encode('utf-8')) #call the Fernet encrypt method
+    return encrypted_data    
 
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)

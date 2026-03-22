c625357255caf9a6cce5d1d231ada58b37fa115b
wu-jiaxi <dev.jiaxiwu@gmail.com>
2026-03-21 23:23:15 -0400

    txt

--- DNSServer.py
+++ DNSServer.py
@@ -44,7 +44,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 salt = "Tandon".encode('utf-8') # Remember it should be a byte-object
-password = "jw9919@nyu.edu"
+password = "jwu9919@nyu.edu"
 input_string = "AlwaysWatching"
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
@@ -149,7 +149,7 @@ dns_records = {
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.CNAME: 'www.nyu.edu.',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-        dns.rdatatype.TXT: (f'"{encrypted_value.decode("utf-8")}"',),
+        dns.rdatatype.TXT: ("AlwaysWatching",),
         dns.rdatatype.SOA: (
             'ns1.nyu.edu.', #mname
             'admin.nyu.edu.', #rname
